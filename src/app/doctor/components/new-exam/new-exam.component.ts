cf781f28221025d9e1d2571f7e4574cc0b2a3552
rafaa1999 <raferhouma10@gmail.com>
2024-07-04 17:29:18 +0100

    Save questions

--- src/app/doctor/components/new-exam/new-exam.component.ts
+++ src/app/doctor/components/new-exam/new-exam.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-new-exam',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewExamComponent implements OnInit {
 
-  constructor() { }
+  name = new FormControl()
+  questionForm!:FormGroup
+  questions:any[] = []
+  correctNum:any
+
+  constructor(private fb:FormBuilder,
+    private toaster:ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.createForm()
+  }
+
+  createForm(){
+    this.questionForm = this.fb.group({
+      question:['', [Validators.required]],
+      answer1:['', [Validators.required]],
+      answer2:['', [Validators.required]],
+      answer3:['', [Validators.required]],
+      answer4:['', [Validators.required]],
+      correctAnswer:['']
+    })
+  }
+
+  submit(){
+    if(this.correctNum){
+      const model = {
+        question:this.questionForm.value.question,
+        answer1:this.questionForm.value.answer1,
+        answer2:this.questionForm.value.answer2,
+        answer3:this.questionForm.value.answer3,
+        answer4:this.questionForm.value.answer4,
+        correctAnswer:this.questionForm.value[this.correctNum]
+      }
+      this.questions.push(model)
+      this.questionForm.reset()
+    }else{
+      this.toaster.error("You should pick the right answer");
+    }
+    console.log(this.questions)
+  }
+
+  createQuestion(){
+    
+  }
+
+  getCorrect(event:any){
+    this.correctNum = event.value
+    console.log(event.value)
   }
 
 }
